66800b5e5a3c183e37cb9d7596d5be6a6b3deadf
Damon-Omand <59703365+Damon-Omand@users.noreply.github.com>
2020-01-09 12:43:19 -0500

    Adding itemClass

---
+++ Item.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author s300006085
+ */
+public class Item {
+      String ID, type, title, dateOrdered, expiryDate, dateAcq,prec,usage,dept, quantity;//values from main document/data collection
+      boolean isExpiring=false,lowStock=false;
+      
+      public Item(String _ID,String _type,String _title, String _dateOrdered,String _expiryDate,String _dateAcq,String _prec,String _usage,String _dept,String _quantity){
+          ID=_ID;//converting values to values in object class 
+          type=_type;
+          title=_title;
+          dateOrdered=_dateOrdered;
+          expiryDate=_expiryDate;
+          dateAcq=_dateAcq;
+          prec=_prec;
+          usage=_usage;
+          dept=_dept;
+          quantity=_quantity;
+}
+      
+}
