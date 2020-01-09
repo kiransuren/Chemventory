32ca5e29a78826fddd490a689868bd3733899c6d
Kiran Suren <kirank.suren@gmail.com>
2020-01-09 14:00:17 -0500

    edit item class, to add desc and note add getItemData to get data from view and create item

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
 public class Controller {
+
+    public TextField idLabel, typeLabel, titleLabel, descLabel, notesLabel, ordDateLabel, expDateLabel, acqDateLabel, precLabel, useLabel, depsLabel, quanLabel;
+    ArrayList<Item> itemArr;
+
+    public void onEnterData(){
+
+
+    }
+
+    public void getItemData(){
+        String id = Integer.toString(Integer.parseInt(idLabel.getText());
+        String type = typeLabel.getText();
+        String title = titleLabel.getText();
+        String desc = descLabel.getText();
+        String notes = notesLabel.getText();
+        String orderDate = ordDateLabel.getText();
+        String expiryDate = expDateLabel.getText();
+        String acqDate = acqDateLabel.getText();
+        String precDate = precLabel.getText();
+        String useDate = useLabel.getText();
+        String department = depsLabel.getText();
+        String quantity = quanLabel.getText();
+
+        Item temp = new Item(id,type, title, desc, notes, orderDate, expiryDate, acqDate, precDate, useDate, department, quantity);
+        itemArr.add(temp);
+
+    }
 }
---
+++ src/sample/Item.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * Damon Omand
+ *
+ */
+
+package sample;
+
+
+public class Item {
+      String ID, type, title, description, notes, dateOrdered, expiryDate, dateAcq,prec,usage,dept,quantity;//values from main document/data collection
+      boolean isExpiring=false,lowStock=false;
+      
+      public Item(String _ID,String _type,String _title, String _description, String _notes, String _dateOrdered, String _expiryDate,String _dateAcq,String _prec,String _usage,String _dept,String _quantity){
+          ID=_ID;//converting values to values in object class
+          type=_type;
+          title=_title;
+          description = _description;
+          notes = _notes;
+          dateOrdered=_dateOrdered;
+          expiryDate=_expiryDate;
+          dateAcq=_dateAcq;
+          prec=_prec;
+          usage=_usage;
+          dept=_dept;
+          quantity=_quantity;
+}}
