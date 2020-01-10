7a016df873233374f7772297fd0d16d45cbf92a1
Kiran Suren <kirank.suren@gmail.com>
2020-01-10 13:32:44 -0500

    fixed xml file reader, writers added xml file modify

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,20 @@
 package sample;
 
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,10 +35,109 @@ public class Controller {
     public void onEnterData(){
         createItem();
         System.out.println(itemArr.get(0));
+        updateXML();
+        resetLocalArray();
+        setItemArray(parseFileToArr());
 
     }
 
-    public void parseFileToArr(){
+    public void resetLocalArray(){
+        itemArr.clear();
+    }
+
+    public void updateXML(){
+        try {
+            try{
+
+                File file = new File("saved_data/inventory.xml");
+
+                if(file.delete()){
+                    System.out.println(file.getName() + " is deleted!");
+                }else{
+                    System.out.println("Delete operation is failed.");
+                }
+
+            }catch(Exception e){
+
+                e.printStackTrace();
+
+            }
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+            Element rootElement = doc.createElement("items");
+            doc.appendChild(rootElement);
+
+            for(int i=0; i<itemArr.size(); i++){
+                Element item = doc.createElement("item");
+
+                Element id = doc.createElement("id");
+                id.setTextContent(itemArr.get(i).ID);
+                item.appendChild(id);
+
+
+                Element type = doc.createElement("type");
+                type.setTextContent(itemArr.get(i).type);
+                item.appendChild(type);
+
+                Element title = doc.createElement("title");
+                title.setTextContent(itemArr.get(i).title);
+                item.appendChild(title);
+
+                Element description = doc.createElement("description");
+                description.setTextContent(itemArr.get(i).description);
+                item.appendChild(description);
+
+                Element notes = doc.createElement("notes");
+                notes.setTextContent(itemArr.get(i).notes);
+                item.appendChild(notes);
+
+                Element orderDate = doc.createElement("orderDate");
+                orderDate.setTextContent(itemArr.get(i).dateOrdered);
+                item.appendChild(orderDate);
+
+                Element expiryDate = doc.createElement("expiryDate");
+                expiryDate.setTextContent(itemArr.get(i).expiryDate);
+                item.appendChild(expiryDate);
+
+                Element acqDate = doc.createElement("acqDate");
+                acqDate.setTextContent(itemArr.get(i).dateAcq);
+                item.appendChild(acqDate);
+
+                Element precautions = doc.createElement("precautions");
+                precautions.setTextContent(itemArr.get(i).prec);
+                item.appendChild(precautions);
+
+                Element usage = doc.createElement("usage");
+                usage.setTextContent(itemArr.get(i).usage);
+                item.appendChild(usage);
+
+                Element departments = doc.createElement("departments");
+                departments.setTextContent(itemArr.get(i).dept);
+                item.appendChild(departments);
+
+                Element quantity = doc.createElement("quantity");
+                quantity.setTextContent(itemArr.get(i).quantity);
+                item.appendChild(quantity);
+
+
+                rootElement.appendChild(item);
+
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File("saved_data/inventory.xml"));
+                transformer.transform(source, result);
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Item[] parseFileToArr(){
         //Creating new document for DOM manipulation
         String filePath = "saved_data/inventory.xml";
         File xmlFile = new File(filePath);                                              //Reference to file at path
@@ -48,25 +149,55 @@ public class Controller {
             Document doc = dBuilder.parse(xmlFile);         //Assigning doc to file at path
             doc.getDocumentElement().normalize();           //Normalize doc
 
-            NodeList items = doc.getElementsByTagName("items");      //Get all course elements and store in nodelist
-            Element tempElement;
-            String[] temp = new String[ITEM_PROPS];
+            NodeList items = doc.getElementsByTagName("item");      //Get all course elements and store in nodelist
+            Item[] outArr = new Item[items.getLength()];
+
             //loop for each course in courses
             for(int i=0; i<items.getLength();i++){
-                tempElement = (Element) items.item(i);                        //Get each course
+                Element tempElement = (Element)items.item(i);
+                System.out.println(tempElement.getTextContent());
+                System.out.println(i);
+                //Fetch Entered Item Data from textfields
+                String id = tempElement.getElementsByTagName("id").item(0).getTextContent();
+                String type = tempElement.getElementsByTagName("type").item(0).getTextContent();
+                String title = tempElement.getElementsByTagName("title").item(0).getTextContent();
+                String description = tempElement.getElementsByTagName("description").item(0).getTextContent();
+                String notes = tempElement.getElementsByTagName("notes").item(0).getTextContent();
+                String orderDate = tempElement.getElementsByTagName("orderDate").item(0).getTextContent();
+                String expiryDate = tempElement.getElementsByTagName("expiryDate").item(0).getTextContent();
+                String acqDate = tempElement.getElementsByTagName("acqDate").item(0).getTextContent();
+                String precautions = tempElement.getElementsByTagName("precautions").item(0).getTextContent();
+                String usage = tempElement.getElementsByTagName("usage").item(0).getTextContent();
+                String departments = tempElement.getElementsByTagName("departments").item(0).getTextContent();
+                String quantity = tempElement.getElementsByTagName("quantity").item(0).getTextContent();
+
+                //Create temporary Item object and initialize properties
+                Item temp = new Item(id,type, title, description, notes, orderDate, expiryDate, acqDate, precautions, usage, departments, quantity);
+                outArr[i] = temp;
             }
+            return outArr;
 
         }catch (SAXException | ParserConfigurationException | IOException e1){
             e1.printStackTrace();
         }
+
+        return null;
 
 
     }
 
 
     public void onSearchItem(){
+        setItemArray(parseFileToArr());
         displayItems();
-        parseFileToArr();
+
+    }
+
+    public void setItemArray(Item[] arr){
+        resetLocalArray();
+        for(int i=0; i<arr.length; i++){
+            itemArr.add(arr[i]);
+        }
     }
 
     public void displayItems(){
@@ -76,7 +207,7 @@ public class Controller {
            -Add filter feature
          */
 
-        invListView.getItems().removeAll();                                     //Clear items from ListView
+        invListView.getItems().clear();                                     //Clear items from ListView
         for(int i=0; i < itemArr.size(); i++){                                  //Loops through each entry in the array and creates a string with data from the i-th entry (concatenation)
             String itemString = itemArr.get(i).getViewString();                 //Gets string of class data
             invListView.getItems().add(itemString);
@@ -98,13 +229,13 @@ public class Controller {
         String orderDate = ordDateLabel.getText();
         String expiryDate = expDateLabel.getText();
         String acqDate = acqDateLabel.getText();
-        String precDate = precLabel.getText();
+        String precautions = precLabel.getText();
         String usage = useLabel.getText();
         String departments = depsLabel.getText();
         String quantity = quanLabel.getText();
 
         //Create temporary Item object and initialize properties
-        Item temp = new Item(id,type, title, description, notes, orderDate, expiryDate, acqDate, precDate, usage, departments, quantity);
+        Item temp = new Item(id,type, title, description, notes, orderDate, expiryDate, acqDate, precautions, usage, departments, quantity);
         //Add Item object to itemArr array
         itemArr.add(temp);
 
--- src/sample/Item.java
+++ src/sample/Item.java
@@ -11,7 +11,7 @@ public class Item {
       String ID, type, title, description, notes, dateOrdered, expiryDate, dateAcq, prec, usage, dept, quantity;//values from main document/data collection
       boolean isExpiring=false,lowStock=false;
       
-      public Item(String _ID,String _type,String _title, String _description, String _notes, String _dateOrdered, String _expiryDate,String _dateAcq,String _prec,String _usage,String _dept,String _quantity){
+      public Item(String _ID, String _type, String _title, String _description, String _notes, String _dateOrdered, String _expiryDate,String _dateAcq,String _prec,String _usage,String _dept,String _quantity){
           ID = _ID;//converting values to values in object class
           type = _type;
           title = _title;
