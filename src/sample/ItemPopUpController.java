6c76d27b666ee933c46b59706ba357613d15f587
Damon <tebly12@gmail.com>
2020-01-12 20:11:51 -0500

    remade controller with tableView- still needs work

--- src/sample/ItemPopUpController.java
+++ src/sample/ItemPopUpController.java
@@ -1,18 +1,60 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class ItemPopUpController<User> {
-    @FXML private TableView<User> tableView;
-    @FXML private TableColumn<User, String>propColm ;
-    @FXML private TableColumn<User, String> infoColm;
-    public void initialize(){
-        propColm.setCellValueFactory(new PropertyValueFactory<User,String>("ID"));
-        //infoColm.setCellValueFactory(new PropertyValueFactory<User, String>());
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class ItemPopUpController{
 
+@FXML private TableView<Item>tableView;
+@FXML private TableColumn<Item, String>propColm;
+@FXML private TableColumn<Item, String>infoColm;
+
+   // @Override
+    public void initialize(URL location, ResourceBundle resources){
+        propColm.setCellValueFactory(new PropertyValueFactory<Item,String>("property"));
+        infoColm.setCellValueFactory(new PropertyValueFactory<Item,String>("information"));
+        tableView.setItems(data);
     }
-}
+
+
+
+    private final ObservableList<Item> data =
+            FXCollections.observableArrayList(
+                    new Item("ID", "15"),
+                    new Item("Type", "Material")
+    );
+
+    public static class Item {
+
+    private final SimpleStringProperty property;
+    private final SimpleStringProperty information;
+
+    private Item(String prop, String info) {
+        this.property = new SimpleStringProperty(prop);
+        this.information = new SimpleStringProperty(info);
+    }
+
+    public String getProperty() {
+        return property.get();
+    }
+
+    public void setProperty(String prop) {
+        property.set(prop);
+    }
+
+    public String getInformation() {
+        return information.get();
+    }
+
+    public void setInformation(String info) {
+        information.set(info);
+    }
+}}
